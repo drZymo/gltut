65da2a8eb2b475ff9bc5dbce91a6cc72067a0450
Ralph Schiedon <ralph@zymo.nl>
2013-01-01 23:18:57 +0100

    Use frustum during rotation

--- HelloWorld/resources/shaders/vertex_shader.glsl
+++ HelloWorld/resources/shaders/vertex_shader.glsl
@@ -11,31 +11,54 @@ out vec2 pass_TextureCoord;
 
 out float fade_factor;
 
-void main(void)
+mat4 view_frustum(float angle_of_view, float aspect_ratio, float z_near, float z_far)
 {
-	const mat4 projection = mat4(
-		vec4(3.0/4.0, 0.0, 0.0, 0.0),
-		vec4(    0.0, 1.0, 0.0, 0.0),
-		vec4(    0.0, 0.0, 0.5, 0.5),
-		vec4(    0.0, 0.0, 0.0, 1.0)
+	return mat4(
+		vec4(1.0/tan(angle_of_view),           0.0, 0.0, 0.0),
+		vec4(0.0, aspect_ratio/tan(angle_of_view),  0.0, 0.0),
+		vec4(0.0, 0.0,    (z_far+z_near)/(z_far-z_near), 1.0),
+		vec4(0.0, 0.0, -2.0*z_far*z_near/(z_far-z_near), 0.0)
 	);
+}
 
-	mat4 rotation = mat4(
+mat4 scale(float x, float y, float z)
+{
+	return mat4(
+		vec4(x,   0.0, 0.0, 0.0),
+		vec4(0.0, y,   0.0, 0.0),
+		vec4(0.0, 0.0, z,   0.0),
+		vec4(0.0, 0.0, 0.0, 1.0)
+	);
+}
+
+mat4 translate(float x, float y, float z)
+{
+	return mat4(
+		vec4(1.0, 0.0, 0.0, 0.0),
+		vec4(0.0, 1.0, 0.0, 0.0),
+		vec4(0.0, 0.0, 1.0, 0.0),
+		vec4(x,   y,   z,   1.0)
+	);
+}
+
+mat4 rotate_x(float theta)
+{
+	return mat4(
 		vec4(1.0,         0.0,         0.0, 0.0),
-		vec4(0.0,  cos(timer),  sin(timer), 0.0),
-		vec4(0.0, -sin(timer),  cos(timer), 0.0),
+		vec4(0.0,  cos(theta),  sin(theta), 0.0),
+		vec4(0.0, -sin(theta),  cos(theta), 0.0),
 		vec4(0.0,         0.0,         0.0, 1.0)
 	);
+}
 
-	mat4 scale = mat4(
-		vec4(4.0/3.0, 0.0, 0.0, 0.0),
-		vec4(    0.0, 1.0, 0.0, 0.0),
-		vec4(    0.0, 0.0, 1.0, 0.0),
-		vec4(    0.0, 0.0, 0.0, 1.0)
-	);
+void main()
+{
+	gl_Position = view_frustum(radians(60.0), 4.0/3.0, 0.5, 5.0)
+		* translate(cos(timer), 0.0, 3.0+sin(timer))
+		* rotate_x(timer)
+		* scale(4.0/3.0, 1.0, 1.0)
+		* in_Position;
 
-	gl_Position = projection * rotation * scale * in_Position;
-	
 	pass_Color = in_Color;
 	pass_TextureCoord = in_TextureCoord;
 	fade_factor = sin(timer) * 0.5 + 0.5;
