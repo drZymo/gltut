6c7ccbeb3af83a65471e0f989032e3459094e7c4
Ralph Schiedon <ralph@zymo.nl>
2013-01-03 23:43:58 +0100

    Pulsate color correctly

--- gltut/resources/shaders/fragment_shader.glsl
+++ gltut/resources/shaders/fragment_shader.glsl
@@ -9,10 +9,12 @@ uniform float time;
 
 const vec4 secondColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
 
+#define TAU  6.2831853071795864769253f
+
 void main()
 {
 	float currTime = mod(time, fragLoopDuration);
-	float currLerp = currTime / fragLoopDuration;
+	float currLerp = sin(TAU * currTime / fragLoopDuration) * 0.5f + 0.5f;
 
 	outputColor = mix(theColor, secondColor, currLerp);
 }
