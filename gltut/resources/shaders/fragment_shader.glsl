47c42440453f56ef3f4ee40cb015b578cb5e48c6
Ralph Schiedon <ralph@zymo.nl>
2013-01-03 16:10:49 +0100

    Play with colors

--- gltut/resources/shaders/fragment_shader.glsl
+++ gltut/resources/shaders/fragment_shader.glsl
@@ -6,6 +6,6 @@ void main()
 {
 	float lerpValue = gl_FragCoord.y / 500.0f;
 
-	outputColor = mix(vec4(1.0f, 1.0f, 1.0f, 1.0f),
-					vec4(0.2f, 0.2f, 0.2f, 1.0f), lerpValue);
+	outputColor = mix(vec4(0.0f, 1.0f, 1.0f, 1.0f),
+					vec4(1.0f, 0.0f, 0.0f, 1.0f), lerpValue);
 }
