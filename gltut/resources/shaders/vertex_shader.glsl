5bdb4b4002e3aeb19441e7c7dc04e48705ea9794
Ralph Schiedon <ralph@zymo.nl>
2013-01-03 23:22:48 +0100

    Use vertex shader to compute offset

--- gltut/resources/shaders/vertex_shader.glsl
+++ gltut/resources/shaders/vertex_shader.glsl
@@ -3,13 +3,21 @@
 in vec4 position;
 in vec4 color;
 
-uniform vec2 offset;
+uniform float time;
+uniform float loopDuration;
 
 smooth out vec4 theColor;
 
 void main()
 {
-	vec4 totalOffset = vec4(offset.x, offset.y, 0.0, 0.0);
+	float timeScale = 3.14159f * 2.0f / loopDuration;
+	float currTime = mod(time, loopDuration);
+	vec4 totalOffset = vec4(
+		cos(currTime * timeScale) * 0.5f,
+		sin(currTime * timeScale) * 0.5f,
+		0.0f,
+		0.0f);
+
 	gl_Position = position + totalOffset;
 	theColor = color;
 }
--- gltut/src/nl/zymo/gltut/Window.java
+++ gltut/src/nl/zymo/gltut/Window.java
@@ -134,7 +134,8 @@ public class Window
 	private int attrib_position;
 	private int attrib_color;
 	
-	private int uniform_offset;
+	private int uniform_time;
+	private int uniform_loopDuration;
 
 	private void createShaderProgram()
 	{
@@ -152,7 +153,8 @@ public class Window
 		attrib_position = GL20.glGetAttribLocation(programId, "position");
 		attrib_color = GL20.glGetAttribLocation(programId, "color");
 		
-		uniform_offset = GL20.glGetUniformLocation(programId, "offset");
+		uniform_time = GL20.glGetUniformLocation(programId, "time");
+		uniform_loopDuration = GL20.glGetUniformLocation(programId, "loopDuration");
 
 		GL20.glDetachShader(programId, vertexShaderId);
 		GL20.glDetachShader(programId, fragmentShaderId);
@@ -171,7 +173,8 @@ public class Window
 		attrib_position = 0;
 		attrib_color = 0;
 		
-		uniform_offset = 0;
+		uniform_time = 0;
+		uniform_loopDuration = 0;
 
 		this.exitOnGLError("destroyShaderProgram");
 	}
@@ -195,15 +198,13 @@ public class Window
 		return shaderId;
 	}
 
-	private float offsetX;
-	private float offsetY;
+	private float time;
+	private float loopDuration;
 	
 	private void logic()
 	{
-		double time = getTime();
-		double theta = (time / 5000) * Math.PI * 2;
-		offsetX = (float) Math.cos(theta) * 0.5f;
-		offsetY = (float) Math.sin(theta) * 0.5f;
+		time = (float)getTime();
+		loopDuration = 5;
 	}
 
 	private void render()
@@ -213,7 +214,8 @@ public class Window
 
 		GL20.glUseProgram(programId);
 		
-		GL20.glUniform2f(uniform_offset, offsetX, offsetY);
+		GL20.glUniform1f(uniform_time, time);
+		GL20.glUniform1f(uniform_loopDuration, loopDuration);
 
 		GL30.glBindVertexArray(vertexArrayObject);
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexBufferObject);
@@ -236,13 +238,13 @@ public class Window
 	}
 
 	/**
-	 * Get the time in milliseconds
+	 * Get the time in seconds
 	 * 
-	 * @return The system time in milliseconds
+	 * @return The system time in seconds
 	 */
 	public double getTime()
 	{
-		return (Sys.getTime() * 1000.0) / Sys.getTimerResolution();
+		return (double)Sys.getTime() / (double)Sys.getTimerResolution();
 	}
 
 	private String ResourceAsString(String ref)
