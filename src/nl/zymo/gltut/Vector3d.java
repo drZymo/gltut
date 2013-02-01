5d21c1df48e4ff78892322b4980eea2810505b4c
drZymo <github@zymo.nl>
2013-02-02 00:13:18 +0100

    Add new classes for double precision Matrix4x4 calculations.

---
+++ src/nl/zymo/gltut/Vector3d.java
@@ -0,0 +1,15 @@
+package nl.zymo.gltut;
+
+public class Vector3d
+{
+	public Vector3d(double x, double y, double z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public final double x;
+	public final double y;
+	public final double z;
+}
---
+++ src/nl/zymo/gltut/Vector4d.java
@@ -0,0 +1,17 @@
+package nl.zymo.gltut;
+
+public class Vector4d
+{
+	public Vector4d(double x, double y, double z, double w)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.w = w;
+	}
+
+	public final double x;
+	public final double y;
+	public final double z;
+	public final double w;
+}
